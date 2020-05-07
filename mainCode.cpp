a90a2fcaa8b8e7b9b6d4ef28d45689a0e0ea9e49
pshivam111 <pshivam111@gmail.com>
2020-05-07 21:48:00 +0530

    rotation of point done

--- mainCode.cpp
+++ mainCode.cpp
@@ -14,8 +14,11 @@ inline double cx(int a)
 {return 960+a; }
 inline double cy(int a)
 {return 508-a; }
-
-float angle(float x,float y)
+double mod(double x, double y)
+{
+    return sqrt(x*x+y*y);
+}
+double angle(double x,double y)
 {
     if(x==0&&y==0)
         return 0;
@@ -23,13 +26,25 @@ float angle(float x,float y)
     {    
     if(x>=0&&y>=0)
         return (57.32*atan(abs(y)/abs(x)));
-    if(x<=0&&y>=0)
+    else if(x<=0&&y>=0)
         return 180-(57.32*atan(abs(y)/abs(x)));
-    if(x<=0&&y<=0)
+    else if(x<=0&&y<=0)
         return 180+(57.32*atan(abs(y)/abs(x)));
-    if(x>=0&&y<=0)
+    else if(x>=0&&y<=0)
         return -(57.32*atan(abs(y)/abs(x)));
+    else
+    return 0;
+    
     }
+}
+
+double getRad(double x,double y)
+{
+    return 3.14*angle(x,y)/180;
+}
+double rad(double x)
+{
+    return 3.14*(x)/180;
 }
 class Point 
 {
@@ -46,33 +61,45 @@ public:
         kk={cx(x__),cy(y__),l__,b__};
         kk.setFill();
     }
+    void printCoor()
+    {
+        cout<<"\n X : "<<x__<<"   Y : "<<y__;
+    }
     void imprint()
     {
         kk.imprint();
     }
-    void translate(int a,int b)
+    void translate(int aa,int ba)
     {
-        x__+=a;
-        y__+=b;
+        printCoor();
+        x__+=aa;
+        y__+=ba;
+        printCoor();
         kk.moveTo(cx(x__),cy(y__));
     }
-    void rotateO(float ang)
+    void rotateO(double ang)
     {
-        x__=angle(x__,y__)+ang
+        printCoor();
+        cout<<"\n angle : "<<angle(x__,y__) <<" ang : "<<ang<<" mod : "<<mod(x__,y__);
+        double tx=x__,ty=y__;
+        x__=mod(tx,ty)*cos(rad(angle(tx,ty)+ang));
+        cout<<"\n x :: "<<x__;
+        y__=mod(tx,ty)*sin(rad(angle(tx,ty)+ang));
+        kk.moveTo(cx(x__),cy(y__));
     }
-   
+
 };
-
-
 
 int main()
 {
 drawCanvas();
-Line w(50,50,51,50);
-Point p(60,60);
-w.imprint();
+// Line w(50,50,51,50);
+Point p(100,0);
+// w.imprint();
 wait(2);
-p.translate(50,50);
+p.translate(50,0);
 wait(2);
-
+p.rotateO(45);
+wait(2);
+p.printCoor();
 }
